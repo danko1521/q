bed34b86a6a0b9098b6effb52b855fe4536b1d43
danko1521 <vip.danko1521@mail.ru>
2021-03-03 23:17:47 +0300

    олег ужас....

---
+++ iter.py
@@ -0,0 +1,39 @@
+import json
+import wikipediaapi
+
+
+
+class ArticleWiki:
+
+    def __init__(self, path, start):
+        self.file = open(path, encoding='UTF-8')
+        self.json = json.load(self.file)
+        self.start = start - 1
+        self.wiki = wikipediaapi.Wikipedia('en')
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.start += 1
+
+
+        if self.start == len(self.json):
+            raise StopIteration
+
+        country = self.json[self.start]['name']['common']
+        country_page = self.wiki.page(country)
+        country_link = country_page.fullurl
+
+        return country, country_link
+
+
+
+if __name__ == '__main__':
+    output_file = open('ebuchaya_viki.txt', 'w', encoding='UTF-8')
+
+    for country, item in ArticleWiki('countries.json', 0):
+        output_file.write(str(country) + '\t = \t' + str(item) + '\n')
+        print('.', end='', flush=True)
+
+    output_file.close()
